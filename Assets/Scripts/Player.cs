c099be8795b3c0f3a95dd0d1a7a73aa045b840ab
zhizhikkkk <zhanmamyt@bk.ru>
2023-09-06 23:06:59 +0600

    /

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -10,14 +10,34 @@ public class Player : MonoBehaviour
     private bool isWalking;
     private void Update()
     {
-
         Vector2 inputVector = gameInput.GetMovementVectorNormalized();
         inputVector = inputVector.normalized;
         Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
 
+        float moveDistance = Time.deltaTime * moveSpeed;
+        float playerRadius = 0.7f;
+        float playerHeight = 2f;
+        bool canMove = !Physics.CapsuleCast(transform.position,transform.position + Vector3.up*playerHeight, playerRadius, moveDir, moveDistance);
+        if (!canMove)
+        {
+            Vector3 moveDirX = new Vector3(moveDir.x, 0, 0).normalized;
+            canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirX, moveDistance);
 
-        float playerSize = 0.7f;
-        bool canMove = !Physics.Raycast(transform.position, moveDir, playerSize);
+            if (canMove)
+            {
+                moveDir = moveDirX;
+            }
+            else
+            {
+                Vector3 moveDirZ = new Vector3(0, 0, moveDir.z).normalized;
+                canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirZ, moveDistance);
+
+                if (canMove)
+                {
+                    moveDir = moveDirZ;
+                }
+            }
+        }
         if (canMove)
         {
             transform.position += moveDir * Time.deltaTime * moveSpeed;
